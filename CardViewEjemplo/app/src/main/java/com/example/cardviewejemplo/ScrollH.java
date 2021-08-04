84d158dbbbc884a70e5177b0b02632a4b12fa0e7
Yeltsin Baquedano <yeltsinbaquedano@gmail.com>
2021-08-04 16:03:55 -0600

    Se agrego ejemplos de cardView a cada opccion en pantalla principal

---
+++ CardViewEjemplo/app/src/main/java/com/example/cardviewejemplo/ScrollH.java
@@ -0,0 +1,13 @@
+package com.example.cardviewejemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScrollH extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_h);
+    }
+}
